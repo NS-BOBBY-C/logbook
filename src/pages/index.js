daf595e10586131d48e61f16440521fd84fdd8ca
robertjcolley <robert.colley04@gmail.com>
2022-10-10 15:30:07 -0400

    Fix deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,44 @@
 import React from "react";
-import BrowserOnly from "@docusaurus/BrowserOnly";
-import Landing from "../components/landing";
+
+const linkStyle = {
+  padding: 8,
+  color: "white",
+};
+
+const links = [
+  { title: "Blog", href: "/blog" },
+  { title: "Programming Blog", href: "/programming-blog" },
+  { title: "Exercise Blog", href: "/exercise-blog" },
+  { title: "Chess Blog", href: "/chess-blog" },
+];
 
 export default function Index() {
   return (
-    <BrowserOnly>
-      <Landing />
-    </BrowserOnly>
+    <div>
+      <div style={{ height: "100vh", backgroundColor: "black" }}>
+        <h1
+          style={{
+            color: "white",
+            fontSize: 600,
+          }}
+        >
+          RJC
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            width: "100%",
+            paddingRight: 64,
+          }}
+        >
+          {links.map((item) => (
+            <a href={item.href} style={linkStyle} key={item.title}>
+              {item.title}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
